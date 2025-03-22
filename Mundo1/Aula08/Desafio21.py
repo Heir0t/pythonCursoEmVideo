b278ad873a7713025b7a77df5f0d3f7b6f5d4455
Heirot <dasilvaheitor871@gmail.com>
2025-03-21 22:53:32 -0300

    Renomeando o arquivo da música

--- Mundo1/Aula08/Desafio21.py
+++ Mundo1/Aula08/Desafio21.py
@@ -2,6 +2,6 @@ import pygame
 
 #Desafio 21 - Faça um programa em python que abra e reproduza um audio de um arquivo mp3
 pygame.init()
-pygame.mixer.music.load('Mundo1/Aula08/OutKast - Hey Ya! (Lyrics).mp3')
+pygame.mixer.music.load('Mundo1/Aula08/heyYa.mp3')
 
 #Com o compilador certo (pycharm). a música ira tocar
